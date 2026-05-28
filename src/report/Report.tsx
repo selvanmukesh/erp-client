b7e9a47701d711a8fdb9cd37c1759b93338caaee
mukesh <mukeshselvan0861@gmail.com>
2026-05-28 17:45:28 +0530

    report module data added

--- src/report/Report.tsx
+++ src/report/Report.tsx
@@ -1,85 +1,85 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { get, post, put } from "../service/apiCall";
+import { nanoid } from "nanoid";
 
 const Report = () => {
 
-  const [name, setName] = useState("");
-  const [orderId, setOrderId] = useState("");
-  const [link, setLink] = useState("");
-  const [allReportData, setAllReportData] = useState<any>([])
+  const [allReportData, setAllReportData] = useState<any[]>([])
+  const saveData = useRef<any[]>([])
+
 
   useEffect(() => {
     fetchAsyncData();
   }, [])
 
-  const handleLogin = async () => {
-    try {
-      const apiUrl = import.meta.env.VITE_ERP_REPORT;
-
-      const url = `${apiUrl}/report`
-      const payload = {
-        name: name,
-        orderNo: parseInt(orderId),
-        link: link
-      }
-
-      const response = await post(url, payload);
-      console.log("response--->", response);
-
-    } catch (error) {
-      console.error("Login Error", error);
-    }
-  };
   const fetchAsyncData = async () => {
-    // reportTable
     const apiUrl = import.meta.env.VITE_ERP_REPORT;
-
     const [reportData]: any = await Promise.allSettled([get(`${apiUrl}/report/reportTable`)]);
-    console.log(reportData.value.data);
-
     if (reportData && reportData?.value?.data) {
-      console.log("reportData----->", reportData);
       setAllReportData(reportData?.value?.data)
     }
   }
-  const handleInputChange = (e: any, id: number) => {
+
+  const handleInputChange = (e: any, id: number | string) => {
     const value = e.target.value?.trim();
     const keyName = e.target.name;
 
     if (keyName) findAndUpdate(id, value, keyName);
   }
-
-  const findAndUpdate = (id: number, value: any, KeyName: string) => {
-    const findIndex = allReportData.findIndex((item: any) => item.id === id);
-    console.log(id, "-----", findIndex);
-
-    if (KeyName === "orderNo") {
-      const valueToInt = parseInt(value);
-      value = Number.isNaN(valueToInt) ? null : valueToInt
-
-    }
-
-    let AllData = allReportData;
-    if (findIndex >= 0) {
-      AllData[findIndex][KeyName] = value;
-      setAllReportData(AllData);
-
-    }
+  const handleDelete = (id: number | string) => {
+    setAllReportData(allReportData.filter(item => item.id !== id));
+    saveData.current = saveData.current.filter(item => item.id !== id)
+    console.log("delete_id", id);
 
   }
 
-  const submit = async () => {
-    // console.log("allReportData---->", allReportData);
 
+  const findAndUpdate = (id: number | string, value: any, keyName: string) => {
+    const toUpdateDataIndex = saveData?.current.findIndex((item: any) => item.id === id);
+    if (toUpdateDataIndex >= 0) {
+      saveData.current[toUpdateDataIndex][keyName] = value;
+    } else {
+      const toUpdateDbDataIndex = allReportData.findIndex((item: any) => item.id === id);
+      if (toUpdateDbDataIndex >= 0) {
+        const data = allReportData[toUpdateDbDataIndex];
+        const payload: any = {
+          id: data?.id || null,
+          name: data.name,
+          orderNo: parseInt(data.orderNo),
+          link: data.link,
+        }
+        payload[keyName] = value;
+        saveData.current.push(payload)
+      }
+
+    }
+  }
+
+  const add = () => {
+    const payload: any = {
+      id: nanoid(),
+      name: null,
+      orderNo: null,
+      link: null
+    }
+    setAllReportData([...allReportData, payload])
+    saveData.current.push(payload)
+  }
+
+
+
+  const submit = async () => {
+
+    const payload = saveData?.current.map(item => ({ ...item, id: typeof item.id === "string" ? null : item.id }))
     try {
       const apiUrl = import.meta.env.VITE_ERP_REPORT;
-
       const url = `${apiUrl}/report`
-
-      const response = await put(url, allReportData);
-      console.log("submit --->", response);
-      // setAllReportData([]);
-
+      const response = await post(url, payload);
+      if (response?.status === 200) {
+        saveData.current = [];
+        setAllReportData([])
+        await fetchAsyncData()
+      }
 
     } catch (error) {
       console.error("Login Error", error);
@@ -92,49 +92,39 @@ const Report = () => {
   return (
     <div>
       <div>
-
-        <h2>Report</h2>
-
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="test"
-          placeholder="Link"
-          value={link}
-          onChange={(e) => setLink(e.target.value)}
-        />
-
-        <br />
-
-        <input
-          type="number"
-          placeholder="Order No"
-          value={orderId}
-          onChange={(e) => setOrderId(e.target.value)}
-        />
-
-        <br />
-
-        <button onClick={handleLogin}>Submit</button>
-
-      </div>
-      <div>
-        <h1>Edit</h1>
-        {allReportData && allReportData.map((item: any) => {
-          return (
-            <div key={item.id}>
-              <input type="text" placeholder="Name" defaultValue={item.name} name="name" onChange={(e) => handleInputChange(e, item.id)} />
-              <input type="text" placeholder="Link" defaultValue={item.link} name="link" onChange={(e) => handleInputChange(e, item.id)} />
-              <input type="number" placeholder="Order No" name="orderNo" defaultValue={item.orderNo} onChange={(e) => handleInputChange(e, item.id)} />
-
-            </div>
-          )
-        })}
-        <button onClick={submit}>Update</button>
+        <button onClick={add}>Add</button>
+        <table cellSpacing={10} width={"100%"}>
+          <thead>
+            <tr>
+              <td>Report Name</td>
+              <td>Link</td>
+              <td>Order No</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            {allReportData && allReportData.map((item: any) => {
+              return (
+                <tr key={item.id || item.customId}>
+                  <td>
+                    <input style={{ width: "100%" }} type="text" placeholder="Name" defaultValue={item.name} name="name" onChange={(e) => handleInputChange(e, item.id)} />
+                  </td>
+                  <td>
+                    <input style={{ width: "100%" }} type="text" placeholder="Link" defaultValue={item.link} name="link" onChange={(e) => handleInputChange(e, item.id)} />
+                  </td>
+                  <td>
+                    <input style={{ width: "100%" }} type="number" placeholder="Order No" name="orderNo" defaultValue={item.orderNo} onChange={(e) => handleInputChange(e, item.id)} />
+                  </td>
+                  <td>
+                    {typeof item.id === "string" ? <button onClick={() => handleDelete(item.id)}>Delete</button> : <></>
+                    }
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <button onClick={submit}>submit</button>
       </div>
 
     </div>
